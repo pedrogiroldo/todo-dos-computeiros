71f81efe7bec26dc7e872eabd67e3816aa80838c
Pedro Giroldo <pedrogiroldo40@gmail.com>
2025-04-11 19:56:22 -0300

    remove all, idc

--- cmake/install-rules.cmake
+++
@@ -1,8 +0,0 @@
-install(
-    TARGETS todo-dos-computeiros_exe
-    RUNTIME COMPONENT todo-dos-computeiros_Runtime
-)
-
-if(PROJECT_IS_TOP_LEVEL)
-  include(CPack)
-endif()
--- test/source/todo-dos-computeiros_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-
-#include "lib.hpp"
-
-TEST_CASE("Name is todo-dos-computeiros", "[library]")
-{
-  auto const lib = library {};
-  REQUIRE(lib.name == "todo-dos-computeiros");
-}
